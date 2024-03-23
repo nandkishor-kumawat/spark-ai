d933fdfd2b25b83b7d4abe249e6c5f0cb9ea896b
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-03-24 00:12:23 +0530

    update sidebar width

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -7,7 +7,7 @@ function Layout({ children, params }: { children: React.ReactNode, params: { id:
     return (
         <div className="w-full h-full">
             <div className="flex w-full h-full">
-                <div className="md:block hidden sidebar min-w-[260px] max-w-sm border-r-zinc-800 border-r h-full">
+                <div className="md:block hidden sidebar min-w-[260px] max-w-[300px] border-r-zinc-800 border-r h-full">
                     <Sidebar />
                 </div>
 
